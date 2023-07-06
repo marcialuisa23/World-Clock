b77f79d0c06bbf001ff369d22b9972e574c56a1e
marcialuisa23 <marcia7monteiro@gmail.com>
2023-07-06 13:47:51 +0100

    adds current date and time of 3 cities

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
+function updateTime() {
+  let firstCityElement = document.querySelector("#first-city");
+  let firstCityDate = firstCityElement.querySelector(".date");
+  let firstCityTime = firstCityElement.querySelector(".time");
+  firstCityDate.innerHTML = moment().format("MMMM Do YYYY");
+  firstCityTime.innerHTML = moment().tz("Asia/Tokyo").format("h:mm:ss A");
 
+  let secondCityElement = document.querySelector("#second-city");
+  let secondCityDate = secondCityElement.querySelector(".date");
+  let secondCityTime = secondCityElement.querySelector(".time");
+  secondCityDate.innerHTML = moment().format("MMMM Do YYYY");
+  secondCityTime.innerHTML = moment().tz("Europe/London").format("h:mm:ss A");
+
+  let thirdCityElement = document.querySelector("#third-city");
+  let thirdCityDate = thirdCityElement.querySelector(".date");
+  let thirdCityTime = thirdCityElement.querySelector(".time");
+  thirdCityDate.innerHTML = moment().format("MMMM Do YYYY");
+  thirdCityTime.innerHTML = moment().tz("America/New_York").format("h:mm:ss A");
+}
+
+updateTime();
+
+setInterval(updateTime, 1000);
